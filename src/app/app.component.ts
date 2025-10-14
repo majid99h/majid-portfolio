f3f60a7bbbc20471861b1856f9324bdebbeb9343
Majid Nawaz <majid@traffic-tech.com>
2025-10-14 16:35:01 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'majid-portfolio';
+export class AppComponent implements OnInit {
+  profileData: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getPortfolioData();
+  }
+
+  getPortfolioData() {
+    this.http.get('assets/data/portfolio.json').subscribe((data) => {
+      this.profileData = data;
+      console.log(this.profileData);
+    });
+  }
 }
